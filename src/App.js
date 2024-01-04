fc8e0201de67ec750d52105506f0b56be39d6d13
Armandohh <armandoramirez568@gmail.com>
2024-01-03 20:47:43 -0800

    Added weekly forecast component

--- src/App.js
+++ src/App.js
@@ -1,17 +1,42 @@
 import './App.css';
 import Search from './components/search/search';
+import CurrentWeather from './components/current-weather/current-weather';
+import Forecast from './components/forecast/forecast';
+import { WEATHER_API_URL, WEATHER_API_KEY } from './api';
+import { useState } from 'react';
 
 function App() {
+  const [currentWeather, setCurrentWeather] = useState(null);
+  const [forecast, setForecast] = useState(null);
 
+  //event handler to log the search data when it changes
   const handleOnSearchChange = (searchData) => {
-    console.log(searchData)
-  }
-  
+    const [lat, lon] = searchData.value.split(" ");
+    const currentWeatherFetch = fetch(`${WEATHER_API_URL}/weather?lat=${lat}&lon=${lon}&appid=${WEATHER_API_KEY}`);
+    const forecastFetch = fetch(`${WEATHER_API_URL}/forecast?lat=${lat}&lon=${lon}&appid=${WEATHER_API_KEY}`);
+
+    Promise.all([currentWeatherFetch, forecastFetch])
+      .then(async (response) => {
+        const weatherResponse = await response[0].json();
+        const forecastResponse = await response[1].json();
+
+        setCurrentWeather({city: searchData.label, ...weatherResponse});
+        setForecast({city: searchData.label, ...forecastResponse});
+      })
+      .catch((err) => console.log(err));
+  };
+
+  console.log(currentWeather);
+  console.log(forecast);
+
+  //render the main app componenet
   return (
     <div className="container">
-      <Search onSearchChange={handleOnSearchChange}/>
+      <Search onSearchChange={handleOnSearchChange} />
+      {currentWeather && <CurrentWeather data={currentWeather}/>}
+      {forecast && <Forecast data={forecast}/>}
     </div>
-  );
+  )
 }
 
 export default App;
--- src/api.js
+++ src/api.js
@@ -6,4 +6,6 @@ export const geoAPIOptions = {
 	}
 };
 
-export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo"
+export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo";
+export const WEATHER_API_URL = `http://api.openweathermap.org/data/2.5`;
+export const WEATHER_API_KEY = "c83ec619f63a2fcf6aeead13f0279e7b";
